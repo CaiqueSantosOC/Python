f448fa91d01536510b155c57f290cc49b047cec9
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-05-09 08:22:18 -0300

    test

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-print ('Hello, world!')
-print ('Muito obrigado!')
-nome = input('Digite o seu nome..')
-sobre = input('Digite seu sobre nome..')
-print (f'Prazer ${nome + sobre}, é um prazer te conheçer')
