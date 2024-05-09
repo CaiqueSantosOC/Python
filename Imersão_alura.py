3a17b249198add744e2316eb13ad48f581c41a00
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-05-09 08:20:51 -0300

    update exercicios python

---
+++ Imersão_alura.py
@@ -0,0 +1,6 @@
+print ('Hello, world!')
+print ('Muito obrigado!')
+nome = input('Digite o seu nome..')
+sobre = input('Digite seu sobre nome..')
+print (f'Prazer ${nome + sobre}, é um prazer te conheçer')
+
