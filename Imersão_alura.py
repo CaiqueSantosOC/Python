83281605e41f100bc18cfd9b0b27ea0ae35a3e5a
Caique Santos <166532520+CaiqueSantosOC@users.noreply.github.com>
2024-05-09 08:27:03 -0300

    correções

--- Imersão_alura.py
+++ Imersão_alura.py
@@ -1,6 +1,6 @@
 print ('Hello, world!')
 print ('Muito obrigado!')
-nome = input('Digite o seu nome..')
-sobre = input('Digite seu sobre nome..')
-print (f'Prazer ${nome + sobre}, é um prazer te conheçer')
+nome = int(input('Digite o seu nome..'))
+sobre = int(input('Digite seu sobre nome..'))
+print (f'Prazer {nome} + {sobre}, é um prazer te conheçer')
 
